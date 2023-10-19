d08b081b34d4549829a0b2f31f628c9bd8c6f90e
Daniel P. Mgawi <54575392+dnlmgwi@users.noreply.github.com>
2023-10-20 01:46:23 +0200

    ussd

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ app.use(express.json());
 // Routes
 app.use("/home", home);
 
+app.use("/ussd", home);
+
 // connection
 const port = process.env.PORT || 9001;
 app.listen(port, () => console.log(`Listening to port ${port}`));
